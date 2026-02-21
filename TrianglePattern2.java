1bca34c40a885dcfad79b708aa934a5763a1c576
Meghanagowd <gowdameghana256@gmail.com>
2026-02-21 22:28:00 +0530

    Day 5: Solved Right-Aligned Triangle Pattern (GFG)
    - Practiced space + star combination logic
    - Improved nested loop control

---
+++ TrianglePattern2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class TrianglePattern2 {
+
+    public static void printPattern(int n) {
+
+        for(int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for(int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        printPattern(n);
+
+        sc.close();
+    }
+}
